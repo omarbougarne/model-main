a6fabd4fde586dd47769c68bc9aec61d6fe96c14
omarbougarne <omarwimu@gmail.com>
2023-10-24 14:14:02 +0100

    burger menu

--- main.js
+++ main.js
@@ -1,25 +1,8 @@
-const mainMenu = document.querySelector('.mainMenu');
-const closeMenu = document.querySelector('.closeMenu');
-const openMenu = document.querySelector('.openMenu');
-const menu_items = document.querySelectorAll('nav .mainMenu li a');
+const menuToggle = document.querySelectorAll('.menu-toggle');
+    const menuDesktop = document.querySelectorAll('.menu-desktop');
 
-
-
-
-openMenu.addEventListener('click',show);
-closeMenu.addEventListener('click',close);
-
-// close menu when you click on a menu item 
-menu_items.forEach(item => {
-    item.addEventListener('click',function(){
-        close();
-    })
-})
-
-function show(){
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
-}
-function close(){
-    mainMenu.style.top = '-100%';
-}
+    menuToggle.forEach((toggle, index) => {
+      toggle.addEventListener('click', () => {
+          menuDesktop[index].classList.toggle('active');
+      });
+  });
