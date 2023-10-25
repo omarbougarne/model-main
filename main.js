2fdd92d7ce901fd4003015fc94ffe833d9303325
omarbougarne <omarwimu@gmail.com>
2023-10-25 10:05:26 +0100

    regex

--- main.js
+++ main.js
@@ -6,3 +6,27 @@ const menuToggle = document.querySelectorAll('.menu-toggle');
           menuDesktop[index].classList.toggle('active');
       });
   });
+  document.getElementById('signup-form').addEventListener('submit', function(event) {
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const usernameRegex = /^.{4,}$/;
+    const emailRegex = /@/; 
+    const passwordRegex = /^.{7,}$/; 
+
+    if (!usernameRegex.test(username)) {
+        alert('Username must be at least 4 characters long');
+        event.preventDefault(); 
+    }
+
+    if (!emailRegex.test(email)) {
+        alert('Email must contain the "@" symbol');
+        event.preventDefault();
+    }
+
+    if (!passwordRegex.test(password)) {
+        alert('Password must be at least 7 characters long');
+        event.preventDefault();
+    }
+});
